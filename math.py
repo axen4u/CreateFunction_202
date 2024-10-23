02744e5462d44e9fff29b04da02ca857463a4945
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-10-23 10:54:11 +0700

    #area : ..... print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {area:.2f}")

--- math.py
+++ math.py
@@ -6,3 +6,6 @@ luas_lingkaran = lambda r: math.pi * r*r
 #contoh penggunaannya
 jari_jari = 7
 area = luas_lingkaran(jari_jari)
+
+#area : .....
+print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {area:.2f}")
